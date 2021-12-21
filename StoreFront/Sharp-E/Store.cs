10b590df0bc37204d7fcc608e6e9e89d63a39517
just1salt <tgbrowning@hotmail.com>
2021-12-21 14:46:46 -0500

    My first Commit

---
+++ StoreFront/Sharp-E/Store.cs
@@ -0,0 +1,17 @@
+    namespace StoreFront;
+
+    public class Store {    //I want to store the name, city and state of store
+
+    //This is property, a type member
+    //Access modifier controls the visibility of type and type members.
+    //There are four access modifiers: Public, Private, Internal, Protected
+    //public is the most visible, private is the least visible
+    //By default, class member has private access modifier
+    //Class, by default, are internal unless you explicitely say otherwise
+    public string Name { get; set; }
+    
+    public string City { get; set; }
+
+    public string State { get; set; }
+
+}
